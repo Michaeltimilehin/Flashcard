9730e3796d1d51159b32b85b41248dc30f5f4083
Michaeltimilehin <adurogboyemichael9@gmail.com>
2024-05-10 09:58:08 +0100

    styling of the box

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
 $(document).ready(function() {
-  $("button1").click(function() {
-    
-    let index = $("button1").index(this);
-
-$(".toxic").hide();
-$(".toxic").eq(index).show();
-$(".toxic").eq(index).find("ul").toxic(); 
-   
+  $("#button1").click(function() {
+    $(".word2").hide()
+    $("#bullet2").toggle()
+  });
+  $("#button3").click(function() {
+    $(".word2").hide()
+    $("#bullet3").toggle() 
+  });
+  $("#button8").click(function() {
+    $(".word2").hide()
+    $("#bullet8").toggle()
+  });
+  $("#button4").click(function() {
+    $(".word2").hide()
+    $("#bullet4").toggle()
+  });
+  $("#button5").click(function() {
+    $(".word2").hide()
+    $("#bullet5").toggle()
+  });
+  $("#button6").click(function() {
+    $(".word2").hide()
+    $("#bullet6").toggle()
+  });
+  $("#button7").click(function() {
+    $(".word2").hide()
+    $("#bullet7").toggle()
+  });
+  $("#button2").click(function() {
+    $(".word2").hide()
+    $("#bullet1").toggle()
+  });
+  $("#button9").click(function() {
+    $(".word2").hide()
+    $("#bullet9").toggle()
+  });
+  $("#button10").click(function() {
+  $(".word2").hide()
+  $("#bullet10").toggle()
   });
 });
